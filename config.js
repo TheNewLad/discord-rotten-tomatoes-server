6eac177cfaac14f1d4e48135ebe5ef72922b3b19
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-05-15 22:20:57 -0500

    chore: remove env.test config

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-import dotenv from "dotenv";
-
-dotenv.config({ path: process.env.NODE_ENV === "test" ? ".env.test" : ".env" });
+import "dotenv/config";
 
 export const env = {
   CLIENT_URL: process.env.CLIENT_URL,
