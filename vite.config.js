9169c3f64c634fdbf20dcc8ae5cf61672a6e7fb0
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-05-30 12:20:55 -0500

    ci: add vitest to project

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vitest/config";
+
+// eslint-disable-next-line no-restricted-exports
+export default defineConfig({
+  test: {
+    files: ["test/**/*.test.js"],
+  },
+});
