53942815703cd9e02d9e6783f1f1f05c7a04ec27
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2024-05-30 20:30:22 -0500

    refactor: update test runner config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import { defineConfig } from "vitest/config";
 // eslint-disable-next-line no-restricted-exports
 export default defineConfig({
   test: {
+    coverage: {
+      include: ["src/**/*.js"],
+    },
     files: ["test/**/*.test.js"],
-    reporter: ["html"],
-    outputFile: "./coverage/report.html",
   },
 });
